ec67a4ed52f72f6ca9d4da9cdd2c9a45f88cd22d
srinivas,vasu <brsvn04322gmail.com>
2019-03-22 12:47:42 +0530

    commit 2

---
+++ network.ps1
@@ -0,0 +1,13 @@
+﻿Get-Command -Module AzureRM.network | Measure-Object
+
+Get-Command -Module AzureRM.Network |Group-Object -Property Noun `
+|Sort-Object -Property name |Select-Object -Property count, name
+
+
+Get-Command -Module AzureRM.network -Noun AzureRmVirtualnetwork 
+
+Get-Command -Module Azurerm.network -Noun AzureRmVirtualnetworksubnetconfig
+
+Get-Command -Module Azurerm.network -Noun AzurerRmNetworksecuritygroup 
+
+Get-Command -Module Azurerm.network -Noun AzureRmNetworkSecurityRuleConfig
